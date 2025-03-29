df728806ddccd8e23aa50bb5be9819748179ebfb
tin2282002@gmail.com <tin2282002@gmail.com>
2025-03-29 08:27:12 +0700

    first commit

---
+++ NguyenThanhTin_2122110125/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace NguyenThanhTin_2122110125.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
